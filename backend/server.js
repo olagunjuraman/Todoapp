20b06f0a281c5fa830cc769b5ae7d20192d04f1c
raman <olagunjuraman@gmail.com>
2021-02-23 07:17:54 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ const __dirnam = Path.resolve();
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 5000,
-    host: "0.0.0.0",
+    port: process.env.PORT,
+    host: "localhost",
     routes: {
       files: {
         relativeTo: Path.join(__dirnam, "/frontend/build"),
